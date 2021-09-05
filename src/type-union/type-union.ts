fcc67af54df919098d3b05f598b869e0e876ab34
nicolaswalcker <walcker_@outlook.com>
2021-09-05 11:23:13 -0300

    Type union

---
+++ src/type-union/type-union.ts
@@ -0,0 +1,6 @@
+function addOrConcat(a: number | string, b: number | string) {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+console.log(addOrConcat(10, 9));
+console.log(addOrConcat('10', 9));
